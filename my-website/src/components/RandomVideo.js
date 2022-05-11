d25193559a900fe440b6142d3a63dec47da4d68e
galegoer <eric.m.galego@gmail.com>
2022-05-10 23:25:43 -0400

    testing initializeparse

--- my-website/src/components/RandomVideo.js
+++ my-website/src/components/RandomVideo.js
@@ -1,6 +1,18 @@
 import React, {useEffect, useState} from "react";
 import Parse from 'parse';
 
+import { initializeParse } from '@parse/react';
+
+const parse_server_url = process.env.REACT_APP_PARSE_SERVER_URL;
+const parse_app_id = process.env.REACT_APP_PARSE_APP_ID;
+const parse_app_js_key = process.env.REACT_APP_PARSE_JS_KEY;
+
+initializeParse(
+  parse_server_url,
+  parse_app_id,
+  parse_app_js_key
+);
+
 function RandomVideo(props) {
     
     const [vidIds, setVidIds] = useState([]); // maybe add default video?
