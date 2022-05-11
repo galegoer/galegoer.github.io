18b3ccaf05eb34796ca6a590a6bc61b61bae7063
galegoer <eric.m.galego@gmail.com>
2022-05-11 16:51:14 -0400

    deploy runs together with build

--- my-website/src/components/RandomVideo.js
+++ my-website/src/components/RandomVideo.js
@@ -1,20 +1,6 @@
 import React, {useEffect, useState} from "react";
 import Parse from 'parse';
 
-import { initializeParse } from '@parse/react';
-import { useParseQuery } from '@parse/react';
-
-const parse_server_url = process.env.REACT_APP_PARSE_SERVER_URL;
-const parse_app_id = process.env.REACT_APP_PARSE_APP_ID;
-const parse_app_js_key = process.env.REACT_APP_PARSE_JS_KEY;
-
-initializeParse(
-  parse_server_url,
-  parse_app_id,
-  parse_app_js_key
-);
-
-
 
 function RandomVideo(props) {
     
@@ -35,25 +21,6 @@ function RandomVideo(props) {
       position: 'absolute'
     };
 
-
-    const query = new Parse.Query('VideoClass');
-    const {
-      isLive, // Indicates that Parse Live Query is connected
-      isLoading, // Indicates that the initial load is being processed
-      isSyncing, // Indicates that the library is getting the latest data from Parse Server
-      results, // Stores the current results in an array of Parse Objects
-      count, // Stores the current results count
-      error, // Stores any error
-      reload // Function that can be used to reload the data
-    } = useParseQuery(
-      query, // The Parse Query to be used
-      {
-        enabled: true, // Enables the parse query (default: true)
-        enableLocalDatastore: true, // Enables cache in local datastore (default: true)
-        enableLiveQuery: true // Enables live query for real-time update (default: true)
-      }
-    );
-
     useEffect(() => {
         provideVid();
     }, []);
@@ -61,7 +28,7 @@ function RandomVideo(props) {
     async function provideVid() {
         // get most recent random video
         // const VideoClass = Parse.Object.extend('VideoClass');
-        // let query = new Parse.Query('VideoClass');
+        let query = new Parse.Query('VideoClass');
         query.ascending('createdAt');
         query.limit(4);
         const videoIds = await query.find();
