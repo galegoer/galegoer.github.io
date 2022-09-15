3e8eb06cc994d71d12c2d713297f332c7681c2e4
galegoer <eric.m.galego@gmail.com>
2022-09-15 16:22:15 -0400

    testing updated workflow with server

--- my-website/client/src/index.js
+++ my-website/client/src/index.js
@@ -21,9 +21,9 @@ initializeParse(
 console.log("test var: ", test_var);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
