c022baa9ea3ae0a6317fd8d9786766f95fea1b9c
galegoer <eric.m.galego@gmail.com>
2025-03-16 23:27:49 -0400

    updating homepage

--- galegoer-github-io/src/components/HelloWorld.vue
+++ galegoer-github-io/src/components/HelloWorld.vue
@@ -8,12 +8,12 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      Here I'll share a little bit about myself and what I've been working on. I'm a software developer from Canada that loves to automate things I do in my day to day life. As you can see from my Github
-      there are many projects however big or small that I've done to simplify the processes of some daily tasks. I'm currently learning Vue and working on a few different projects. You will see these under my WIP section under the Projects tab.
-      I've also been inspired to write some blog posts, I've never been a big writer but I find it helps to communicate your thoughts into words not only for technical posts via learning but also for life.
-      Thanks for stopping by! Feel free to reach out here!
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Here I'll share a little bit about myself and what I've been working on. I'm a software developer from Canada that loves to automate things I do in my day to day life. As you may have seen from my Github
+      there are many projects that I've done to simplify the processes of some daily tasks. I'm currently learning Vue and working on a few different projects. You will see these under the WIP section in the Projects tab.
+      I've also been inspired to write some blog posts, so you'll see some soon!
+      Feel free to reach out through the Contact page!
+      <br/>
+      Thanks for stopping by!
     </h3>
   </div>
 </template>
--- galegoer-github-io/src/views/HomeView.vue
+++ galegoer-github-io/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import IconHammerWrench from '@/components/icons/IconHammerWrench.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h2>
+        <IconHammerWrench />
+        NOTE: This is a simple Vue template, updates are coming soon! The content on this site is still a WIP!
+        <IconHammerWrench />
+    </h2>
 </template>
+
+<style scoped>
+
+h2 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  h2 {
+    text-align: center;
+  }
+}
+</style>
